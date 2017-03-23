dd5c73648333b3f0b496b58603d976c6c039bad5
Adriaan <adriaan.rol@gmail.com>
2017-03-23 20:31:57 +0100

    Initial empty commit

---
+++ autodepgraph/tests/test_basic.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+# class TestDataArray(TestCase):
+
+
+def test_attributes():
+    pass
---
+++ setup.py
@@ -0,0 +1,14 @@
+
+import setuptools
+from distutils.core import setup
+
+
+setup(name='autodepgraph',
+      version='0.1',
+      description='automated tuning based on dependency graph',
+      author='Adriaan Rol et al',
+      author_email='adriaan.rol@gmail.com',
+      packages=['autodepgraph'],
+      ext_package='autodepgraph',
+      requires=["pytools", "numpy(>=1.12)", "pytest", "matplotlib"]
+      )
