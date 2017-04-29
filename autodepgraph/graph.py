9aa30d1315503de9fcf1c6eb0898b891e7cce454
Adriaan <adriaan.rol@gmail.com>
2017-04-29 18:42:08 +0200

    Removed doubly defined calibration node class and addressed codacy comments

--- autodepgraph/graph.py
+++ autodepgraph/graph.py
@@ -1,9 +1,8 @@
 
 from qcodes.instrument.base import Instrument
-from qcodes.instrument.parameter import ManualParameter
-from qcodes.utils import validators as vals
 import yaml
 import logging
+from autodepgraph.node import CalibrationNode
 
 
 class Graph(Instrument):
@@ -20,7 +19,7 @@ class Graph(Instrument):
         Loads a graph.
         """
         graph_snap = yaml.safe_load(open(filename, 'r'))
-        for key, node_snap in graph_snap['nodes'].items():
+        for node_snap in graph_snap['nodes'].values():
             try:
                 # Look for an existing node
                 node = self.find_instrument(node_snap['name'])
@@ -53,7 +52,7 @@ class Graph(Instrument):
     def snapshot(self, update=False):
         snap = {'nodes': {},
                 'meta': {}}
-        for nodename, node in self._nodes.items():
+        for node in self._nodes.values():
             snap['nodes'][node.name] = node.snapshot(update=update)
         return snap
 
@@ -61,55 +60,3 @@ class Graph(Instrument):
         if node.name in self._nodes.keys():
             logging.warning('Node already exists in graph')
         self._nodes[node.name] = node
-
-
-class CalibrationNode(Instrument):
-
-    def __init__(self, name):
-        super().__init__(name)
-        self.add_parameter('state', parameter_class=ManualParameter,
-                           docstring='',
-                           vals=vals.Enum('good', 'needs calibration',
-                                          'bad', 'unknown', 'active'),
-                           initial_value='unknown')
-
-        self.add_parameter('dependencies',
-                           docstring='a list of names of Calibration nodes',
-                           vals=vals.Lists(vals.Strings()),
-                           parameter_class=ManualParameter)
-        self.add_parameter('check_functions',
-                           docstring='Name of the function that corresponds to checking the node',
-                           vals=vals.Lists(vals.Strings()),
-                           parameter_class=ManualParameter)
-        self.add_parameter('calibrate_functions',
-                           docstring='Name of the function that calibrating the node',
-                           vals=vals.Lists(vals.Strings()),
-                           parameter_class=ManualParameter)
-
-    def __call__(self):
-        self.execute_node()
-
-    def execute_node(self):
-        """
-        Contains the logic of the nodes will have
-        """
-        state = self.state()
-        if state == 'good':
-            print('Node is good')
-            return True
-        if self.state == 'bad':
-            self.check_dependencies()
-        self.calibrate()
-        return self.state()
-
-    def check_dependencies(self):
-        return True
-
-    def calibrate(self):
-        print('calibrating '+self.name)
-        self.state('good')  # set the state to good
-        return True
-
-    def check(self):
-        self.state('needs calibration')
-        return self.state()
--- autodepgraph/tests/test_basic.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-import autodepgraph as adg
-# class TestDataArray(TestCase):
-
-
-def test_attributes():
-    pass
