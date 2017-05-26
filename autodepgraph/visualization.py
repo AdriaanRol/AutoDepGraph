dead2753611e677418b8e1c0abc308c17907e246
Adriaan <adriaan.rol@gmail.com>
2017-05-26 13:12:22 +0200

    Working pyqtgraph based visualization
    next step integrate it in the executing of the program

--- autodepgraph/visualization.py
+++ autodepgraph/visualization.py
@@ -1,5 +1,8 @@
 import networkx as nx
-
+import numpy as np
+import pyqtgraph as pg
+from autodepgraph.pg_visualization import pg_DirectedGraphItem as pgG
+from autodepgraph.pg_visualization.pg_remotegraph import pg_DiGraph_window
 
 # Colormap used to map states to node colors
 state_cmap = {'unknown': '#7f7f7f',            # middle gray
@@ -73,3 +76,59 @@ def draw_graph_mpl(snapshot, pos=None, layout='spring'):
     nx.draw_networkx_edges(nxG, pos, arrows=True)
     nx.draw_networkx_labels(nxG, pos)
     return pos
+
+
+def adjaceny_to_integers(nxG):
+    """
+    Helper function that takes a networkx graph object and returns the
+    edges (adjacency) as an array of integers.
+
+    Args:
+        nxG : networkx graph object
+    returns:
+        adj : array of integers specifying the edges
+    """
+    adj = []
+    for child, parent in nxG.edges():
+        child_idx = nxG.nodes().index(child)
+        parent_idx = nxG.nodes().index(parent)
+        adj.append([child_idx, parent_idx])
+    adj = np.array(adj)
+    return adj
+
+
+def draw_graph_pyqt(snapshot, DiGraphWindow=None, pos=None, layout='spring'):
+    """
+    Function to create a quick plot of a graph using matplotlib.
+    Intended mostly for for debugging purposes
+    Args:
+        snapshot : snapshot of the graph
+        DiGraphWindow     : pyqtgraph remote graph window to be updated
+            if None it will create a new plotting window to update
+        layout (str) : layout to position the nodes options are:
+            spring, shell, spectral and circular.
+    returns:
+        pos
+
+    """
+    nxG = snapshot_to_nxGraph(snapshot)
+
+    # Converts it to an array to work with pyqtgraph plotting class
+    pos = nx.nx_agraph.graphviz_layout(nxG, prog='dot')
+    pos = np.array(list(pos.values()))
+    adj = adjaceny_to_integers(nxG)
+
+    # Edge colors need to be set using a value mapping and a cmap
+    cm = get_state_col_map(snapshot)
+    colors_list = [(cm[node]) for node in nxG.nodes()]
+
+    symbols = ['o']*len(pos)
+    labels = list(nxG.nodes())
+
+    if DiGraphWindow is None:
+        DiGraphWindow = pg_DiGraph_window()
+
+    DiGraphWindow.setData(pos=np.array(pos), adj=adj, size=20, symbol=symbols,
+                     labels=labels, pen=(60, 60, 60),
+                     symbolBrush=colors_list, pxMode=False)
+    return DiGraphWindow
